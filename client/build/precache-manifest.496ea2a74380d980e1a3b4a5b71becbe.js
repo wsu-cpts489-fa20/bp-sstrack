8785a8263e60b37d6d6b8ef48b2f8adfc98cf210
Connor P. SD-20 <pittconnor2@gmail.com>
2020-11-13 08:46:36 -0800

    Implemented google/facebook logins via ch.20 code

--- client/build/precache-manifest.496ea2a74380d980e1a3b4a5b71becbe.js
+++ client/build/precache-manifest.496ea2a74380d980e1a3b4a5b71becbe.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "807e1cb55beb7dd17dddbb5244c890a9",
+    "revision": "6e74275727cb30ef899ba895cbbe22d9",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.d653346d.chunk.css"
   },
   {
-    "revision": "209987a794ab9b08c9a1",
+    "revision": "775aa73553aab9c6c185",
     "url": "/static/css/main.6a8397fb.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.928e17f6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "209987a794ab9b08c9a1",
-    "url": "/static/js/main.8e64184a.chunk.js"
+    "revision": "775aa73553aab9c6c185",
+    "url": "/static/js/main.e384c9de.chunk.js"
   },
   {
     "revision": "19793d9fa96413d1c44d",
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d4dfbfc5e4e9d50289f1e3304de995fa.js"
+  "/precache-manifest.496ea2a74380d980e1a3b4a5b71becbe.js"
 );
 
 self.addEventListener('message', (event) => {

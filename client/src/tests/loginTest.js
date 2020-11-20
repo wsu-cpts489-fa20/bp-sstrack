d68c939c4b1c25d17a37df8b2fca2bbfabcb0ca5
Alex Puga <alex.puga@wsu.edu>
2020-11-19 17:31:41 -0800

    current code for testing

--- client/src/tests/loginTest.js
+++ client/src/tests/loginTest.js
@@ -1,5 +1,6 @@
 import { Selector } from 'testcafe';
 
+
 fixture `SS-Track`
     .page `http://localhost:8081/`;
 
@@ -9,10 +10,14 @@ test('Login goes to main page', async t => {
         const emailInput = Selector('#emailInput');
         const emailPW = Selector('#passwordInput')
     await t
+        // Enters text
         .typeText(emailInput, email)
+        .pressKey('tab')
         .typeText(emailPW,pw)
+        // Checks expected input
         .expect(emailInput.value).eql(email)
         .expect(emailPW.value).eql(pw)
+        // Logs in and checks if coursePage is displayed
         .click('#loginBtn')
         .expect(Selector('#coursePage').visible).eql(true)
 });
